65aae3c3d6c940ddfb793a22e95a15714031f6f6
bunbunbunzai <99793483+bunbunbunzai@users.noreply.github.com>
2023-10-13 00:12:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let users = ["User1", "User2"];
-let currentSelection = 'o';
+let users = [];  // No pre-added users
+let currentSelection = '〇';
+let isDragging = false;
 
 function addUser() {
   const newUser = document.getElementById("newUser").value;
@@ -23,37 +24,43 @@ function createCalendarForDate(date) {
   
   const table = document.createElement("table");
   const tableBody = document.createElement("tbody");
-  table.id = `calendar-${date}`;
   
   const header = document.createElement("tr");
-  const timeHeader = document.createElement("th");
-  timeHeader.textContent = "Time";
-  header.appendChild(timeHeader);
-  users.forEach(user => {
-    const userHeader = document.createElement("th");
-    userHeader.textContent = user;
-    header.appendChild(userHeader);
-  });
-  tableBody.appendChild(header);
+  const userHeader = document.createElement("th");
+  userHeader.textContent = "User";
+  header.appendChild(userHeader);
 
   for (let hour = 0; hour < 24; hour++) {
     ["00", "30"].forEach(minute => {
-      const row = document.createElement("tr");
-      const timeCell = document.createElement("td");
-      timeCell.textContent = `${String(hour).padStart(2, "0")}:${minute}`;
-      row.appendChild(timeCell);
-
-      users.forEach(() => {
+      const timeHeader = document.createElement("th");
+      timeHeader.textContent = `${String(hour).padStart(2, "0")}:${minute}`;
+      timeHeader.className = 'rotate';
+      header.appendChild(timeHeader);
+    });
+  }
+  tableBody.appendChild(header);
+  
+  users.forEach(user => {
+    const row = document.createElement("tr");
+    const userNameCell = document.createElement("td");
+    userNameCell.textContent = user;
+    row.appendChild(userNameCell);
+    
+    for (let hour = 0; hour < 24; hour++) {
+      ["00", "30"].forEach(() => {
         const cell = document.createElement("td");
         cell.addEventListener("click", function() {
-          this.className = currentSelection;
+          this.textContent = currentSelection;
+        });
+        cell.addEventListener("mouseover", function() {
+          if (isDragging) this.textContent = currentSelection;
         });
         row.appendChild(cell);
       });
-      
-      tableBody.appendChild(row);
-    });
-  }
+    }
+    
+    tableBody.appendChild(row);
+  });
   
   table.appendChild(tableBody);
   dateDiv.appendChild(table);
@@ -63,21 +70,11 @@ function createCalendarForDate(date) {
 function generateTables() {
   const startDate = new Date(document.getElementById("startDate").value);
   const endDate = new Date(document.getElementById("endDate").value);
-
   if (startDate > endDate) {
     alert("開始日は終了日より前でなければなりません。");
     return;
   }
-
   const datesDiv = document.getElementById("dates");
-  datesDiv.innerHTML = "";
+  dates
 
-  for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
-    const dateString = date.toISOString().split("T")[0];
-    createCalendarForDate(dateString);
-  }
-}
-
-// 初期化
-generateTables();
 
