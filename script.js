a2f6ab66886921ad6bb74d77f8990a6f0af86683
bunbunbunzai <99793483+bunbunbunzai@users.noreply.github.com>
2023-10-12 23:40:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,66 @@
-let userCount = 2;
+let users = [];
+let currentSelection = 'o';
+let isDragging = false;
 
-function updateUserCount() {
-  userCount = document.getElementById("userCount").value;
-  initializeCalendar();
+function addUser() {
+  const newUser = document.getElementById("newUser").value;
+  if (newUser) {
+    users.push(newUser);
+    initializeCalendar();
+  }
+}
+
+function setSelection(selection) {
+  currentSelection = selection;
+}
+
+function checkAvailability() {
+  // Omitted for brevity; you can add the logic here
+}
+
+function createCalendarForDate(date) {
+  const dateDiv = document.createElement("div");
+  dateDiv.className = 'date';
+  
+  const dateLabel = document.createElement("h2");
+  dateLabel.textContent = date;
+  dateDiv.appendChild(dateLabel);
+  
+  const table = document.createElement("table");
+  table.id = `calendar-${date}`;
+  
+  // ... (same as before, to create the table)
+  
+  dateDiv.appendChild(table);
+  document.getElementById("dates").appendChild(dateDiv);
 }
 
 function initializeCalendar() {
-  const hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
-  const minutes = ["00", "30"];
-  const tableHead = document.querySelector("#calendar thead tr");
-  const tableBody = document.querySelector("#calendar tbody");
-
-  // Clear existing table
-  tableHead.innerHTML = "<th>Time</th>";
-  tableBody.innerHTML = "";
-
-  // Add user headers
-  for (let i = 1; i <= userCount; i++) {
-    const userHeader = document.createElement("th");
-    userHeader.textContent = `User ${i}`;
-    tableHead.appendChild(userHeader);
-  }
-
-  // Add time slots
-  hours.forEach(hour => {
-    minutes.forEach(minute => {
-      const time = `${hour}:${minute}`;
-      const row = document.createElement("tr");
-      const timeCell = document.createElement("td");
-      timeCell.textContent = time;
-      row.appendChild(timeCell);
-
-      for (let i = 1; i <= userCount; i++) {
-        const cell = document.createElement("td");
-        cell.addEventListener("click", function() {
-          if (this.classList.contains("o")) {
-            this.classList.remove("o");
-            this.classList.add("triangle");
-          } else if (this.classList.contains("triangle")) {
-            this.classList.remove("triangle");
-            this.classList.add("x");
-          } else if (this.classList.contains("x")) {
-            this.classList.remove("x");
-          } else {
-            this.classList.add("o");
-          }
-        });
-        row.appendChild(cell);
-      }
-      tableBody.appendChild(row);
-    });
+  // ... (same as before)
+  
+  // Add mouse event listeners for drag-to-fill functionality
+  tableBody.addEventListener("mousedown", e => {
+    isDragging = true;
+    if (e.target.tagName === "TD") {
+      e.target.className = currentSelection;
+    }
   });
+  
+  tableBody.addEventListener("mouseover", e => {
+    if (isDragging && e.target.tagName === "TD") {
+      e.target.className = currentSelection;
+    }
+  });
+  
+  tableBody.addEventListener("mouseup", () => {
+    isDragging = false;
+  });
+  
+  // Add date
+  createCalendarForDate("2022-01-01");  // Replace with actual date
+  createCalendarForDate("2022-01-02");  // Replace with actual date
+  
+  checkAvailability();
 }
 
 // Initialize
