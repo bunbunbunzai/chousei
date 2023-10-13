d30630af1f217c9b7e4c858c7a3e99ea80d2fafc
bunbunbunzai <99793483+bunbunbunzai@users.noreply.github.com>
2023-10-13 15:17:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ let currentSelection = '〇';
 let isDragging = false;
 let cellData = {};
 
+// Load data from localStorage on page load
 document.addEventListener('DOMContentLoaded', function() {
-  // Load data from localStorage
   const storedData = JSON.parse(localStorage.getItem('cellData') || '{}');
   const storedUsers = JSON.parse(localStorage.getItem('users') || '[]');
   cellData = storedData;
   users = storedUsers;
-
+  
   const today = new Date().toISOString().split('T')[0];
   document.getElementById('startDate').value = today;
   document.getElementById('endDate').value = today;
-
+  
   generateTables();
 });
 
@@ -23,6 +23,7 @@ function saveDataToLocalStorage() {
   localStorage.setItem('users', JSON.stringify(users));
 }
 
+// Add new user
 function addUser() {
   const newUser = document.getElementById("newUser").value;
   if (newUser && !users.includes(newUser)) {
@@ -32,17 +33,12 @@ function addUser() {
   }
 }
 
-function resetAll() {
-  cellData = {};
-  users = [];
-  generateTables();
-  saveDataToLocalStorage();
-}
-
+// Set current selection for cell marking
 function setSelection(selection) {
   currentSelection = selection;
 }
 
+// Toggle cell marking
 function toggleCell(cell, date, user, time) {
   cell.classList.remove('cell-o', 'cell-triangle', 'cell-x');
   if (cell.textContent === currentSelection) {
@@ -60,6 +56,7 @@ function toggleCell(cell, date, user, time) {
   saveCellData(date, user, time, cell.textContent);
 }
 
+// Save data to the cellData object
 function saveCellData(date, user, time, value) {
   if (!cellData[date]) {
     cellData[date] = {};
@@ -71,12 +68,25 @@ function saveCellData(date, user, time, value) {
   saveDataToLocalStorage();
 }
 
+// Load data from the cellData object
+function loadCellData(date, user, time) {
+  return cellData[date] && cellData[date][user] && cellData[date][user][time] || '';
+}
+
+// Create calendar for a specific date
+function createCalendarForDate(date) {
+  // (Your existing code to create the calendar)
+  // At the end, load cell data using loadCellData()
+}
+
+// Generate tables for the date range
 function generateTables() {
-  // ... (Your existing code for generating tables)
-  // At the end of the function, call findCommonTimes()
+  // (Your existing code to generate tables)
+  // At the end, call findCommonTimes()
   findCommonTimes();
 }
 
+// Find common times where all users have marked '〇'
 function findCommonTimes() {
   const commonTimes = [];
   const dates = Object.keys(cellData);
@@ -90,3 +100,15 @@ function findCommonTimes() {
   }
   document.getElementById('commonTimes').textContent = commonTimes.join(', ');
 }
+
+// Reset all tables, cellData, and users
+function resetAll() {
+  cellData = {};
+  users = [];
+  generateTables();
+  saveDataToLocalStorage();
+}
+
+document.addEventListener("mousedown", () => { isDragging = true; });
+document.addEventListener("mouseup", () => { isDragging = false; });
+
