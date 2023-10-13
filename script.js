45c1d1fa478a0248a3ed22efe9adbd49b4034a87
bunbunbunzai <99793483+bunbunbunzai@users.noreply.github.com>
2023-10-13 15:24:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -33,12 +33,10 @@ function addUser() {
   }
 }
 
-// Set current selection for cell marking
 function setSelection(selection) {
   currentSelection = selection;
 }
 
-// Toggle cell marking
 function toggleCell(cell, date, user, time) {
   cell.classList.remove('cell-o', 'cell-triangle', 'cell-x');
   if (cell.textContent === currentSelection) {
@@ -56,7 +54,6 @@ function toggleCell(cell, date, user, time) {
   saveCellData(date, user, time, cell.textContent);
 }
 
-// Save data to the cellData object
 function saveCellData(date, user, time, value) {
   if (!cellData[date]) {
     cellData[date] = {};
@@ -68,47 +65,91 @@ function saveCellData(date, user, time, value) {
   saveDataToLocalStorage();
 }
 
-// Load data from the cellData object
 function loadCellData(date, user, time) {
   return cellData[date] && cellData[date][user] && cellData[date][user][time] || '';
 }
 
-// Create calendar for a specific date
-function createCalendarForDate(date) {
-  // (Your existing code to create the calendar)
-  // At the end, load cell data using loadCellData()
-}
-
-// Generate tables for the date range
 function generateTables() {
-  // (Your existing code to generate tables)
-  // At the end, call findCommonTimes()
-  findCommonTimes();
-}
+  const datesDiv = document.getElementById("dates");
+  datesDiv.innerHTML = "";
 
-// Find common times where all users have marked '〇'
-function findCommonTimes() {
-  const commonTimes = [];
-  const dates = Object.keys(cellData);
-  for (const date of dates) {
-    const timeSlots = Object.keys(cellData[date][users[0]] || {});
-    for (const time of timeSlots) {
-      if (users.every(user => cellData[date][user] && cellData[date][user][time] === '〇')) {
-        commonTimes.push(`${date} ${time}`);
-      }
-    }
+  const startDate = new Date(document.getElementById("startDate").value);
+  const endDate = new Date(document.getElementById("endDate").value);
+  
+  for (let date = new Date(startDate); date <= endDate; date.setDate(date.getDate() + 1)) {
+    const dateString = date.toISOString().split("T")[0];
+    createCalendarForDate(dateString);
   }
-  document.getElementById('commonTimes').textContent = commonTimes.join(', ');
 }
 
-// Reset all tables, cellData, and users
+function createCalendarForDate(date) {
+  const dateDiv = document.createElement("div");
+  dateDiv.className = 'date';
+  
+  const dateLabel = document.createElement("h2");
+  dateLabel.textContent = date;
+  dateDiv.appendChild(dateLabel);
+  
+  const table = document.createElement("table");
+  const tableBody = document.createElement("tbody");
+  
+  const header = document.createElement("tr");
+  const timeHeader = document.createElement("th");
+  timeHeader.textContent = "Time";
+  header.appendChild(timeHeader);
+
+  for (const user of users) {
+    const userHeader = document.createElement("th");
+    userHeader.textContent = user;
+    header.appendChild(userHeader);
+  }
+  
+  tableBody.appendChild(header);
+  
+  for (let hour = 0; hour < 24; hour++) {
+    ["00", "30"].forEach(minute => {
+      const row = document.createElement("tr");
+      const timeCell = document.createElement("td");
+      timeCell.textContent = `${String(hour).padStart(2, "0")}:${minute}`;
+      row.appendChild(timeCell);
+      
+      users.forEach(user => {
+        const cell = document.createElement("td");
+        cell.addEventListener("click", function() {
+          toggleCell(this, date, user, `${hour}:${minute}`);
+        });
+        cell.addEventListener("mouseover", function() {
+          if (isDragging) {
+            toggleCell(this, date, user, `${hour}:${minute}`);
+          }
+        });
+        cell.textContent = loadCellData(date, user, `${hour}:${minute}`);
+        if (cell.textContent === '〇') {
+          cell.classList.add('cell-o');
+        } else if (cell.textContent === '△') {
+          cell.classList.add('cell-triangle');
+        } else if (cell.textContent === '×') {
+          cell.classList.add('cell-x');
+        }
+        row.appendChild(cell);
+      });
+      
+      tableBody.appendChild(row);
+    });
+  }
+  
+  table.appendChild(tableBody);
+  dateDiv.appendChild(table);
+  document.getElementById("dates").appendChild(dateDiv);
+}
+
+document.addEventListener("mousedown", () => { isDragging = true; });
+document.addEventListener("mouseup", () => { isDragging = false; });
+
 function resetAll() {
   cellData = {};
   users = [];
   generateTables();
   saveDataToLocalStorage();
 }
-
-document.addEventListener("mousedown", () => { isDragging = true; });
-document.addEventListener("mouseup", () => { isDragging = false; });
 
